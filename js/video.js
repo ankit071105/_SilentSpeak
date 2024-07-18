5e4c05bf5f64d72d2da731a1099c679da1401514
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-07-18 15:40:15 +0530

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById("autoplay-video");
+    const playBtn = document.getElementById("play-btn");
+    const pauseBtn = document.getElementById("pause-btn");
+    const stopBtn = document.getElementById("stop-btn");
+  
+    playBtn.addEventListener("click", function() {
+      video.play();
+    });
+  
+    pauseBtn.addEventListener("click", function() {
+      video.pause();
+    });
+  
+    stopBtn.addEventListener("click", function() {
+      video.pause();
+      video.currentTime = 0;
+    });
+    video.addEventListener("ended", function() {
+      video.pause(); 
+    });
+  });
+  
